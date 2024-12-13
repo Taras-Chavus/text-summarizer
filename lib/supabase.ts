615c676129af93285567e2d5e25bc8b44b7dec89
Taras <chavus.taras@gmail.com>
2024-12-13 17:48:20 +0200

    bug with supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
+const supabaseUrl = "https://dxmjwszeqejgkmvfqyxf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4bWp3c3plcWVqZ2ttdmZxeXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NTY5NTcsImV4cCI6MjA0OTMzMjk1N30.zU-gmvC2-F1PffFbQd1vEmgpYVAodFSi1J7CeUgd71g";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
